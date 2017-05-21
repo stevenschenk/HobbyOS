c531e91986895f00df86bdc49c60d4a4dbeb11b9
Steven Schenk <stevenschenk74@hotmail.com>
2017-05-21 16:16:11 +0200

    rewrote the boot.s file into loader.s (syntax change + minor tweaks)

--- loader.s
+++ loader.s
@@ -1,11 +1,12 @@
 extern kernel_main
 
-ALIGNMENT	equ 1<<0
-MEMINFO 	equ 1<<1
-FLAGS		equ ALIGNMENT | MEMINFO
-MAGIC		equ 0x1BADB002
-CHECKSUM	equ -(MAGIC + FLAGS)
+ALIGNMENT	equ 1<<0			; Set alignment to flag to true
+MEMINFO 	equ 1<<1			; Set Meminfo flag to true
+FLAGS		equ ALIGNMENT | MEMINFO		; Compute flag
+MAGIC		equ 0x1BADB002			; Magic number for the bootloader
+CHECKSUM	equ -(MAGIC + FLAGS)		; CHECKSUM + MAGIC + FLAGS = 0
 
+; Write the multiboot header in memory
 section .text
 global _start
 align 4
@@ -13,8 +14,9 @@ align 4
     dd FLAGS
     dd CHECKSUM
 
-KERNEL_STACK_SIZE equ 0x1000 ;4KiB
+KERNEL_STACK_SIZE equ 0x1000 ; Stack size of 4KiB
 
+; Reserve bytes for the stack
 section .bss
 align 4
 kernel_stack:
@@ -22,8 +24,10 @@ kernel_stack:
 
 section .text
 _start:
-	mov esp, kernel_stack + KERNEL_STACK_SIZE ;x86 stacks points at end
-	call kernel_main
+	mov esp, kernel_stack + KERNEL_STACK_SIZE 	; Set the stack pointer to the bottom 
+							; of the stack, since x86 is bottom
+							; to top.
+	call kernel_main				
 	
-loop:	
+loop:							
 	jmp loop
